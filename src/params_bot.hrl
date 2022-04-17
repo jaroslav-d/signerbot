aab3429163b7a0ec1880fcc070cae1ff86f8703b
jaroslav <drozdovji@yandex.ru>
2022-04-18 00:15:20 +0300

    example

---
+++ src/params_bot.hrl
@@ -0,0 +1,12 @@
+%%%-------------------------------------------------------------------
+%%% @author jaroslav
+%%% @copyright (C) 2022, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 17. Apr 2022 21:41
+%%%-------------------------------------------------------------------
+-author("jaroslav").
+
+-define(PASSWORD, <<"my_password">>).
+-define(TOKEN, "my_telegram_token").
